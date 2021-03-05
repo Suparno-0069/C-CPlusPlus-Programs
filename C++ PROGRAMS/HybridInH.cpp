b6486e3be38599736dcafb8407bb0a5d0c117aed
Suparno-0069 <suparnochakraborty69@gmail.com>
2021-03-05 17:08:45 +0530

    05MAR#3

--- C++ PROGRAMS/HybridInH.cpp
+++ C++ PROGRAMS/HybridInH.cpp
@@ -6,60 +6,90 @@ class Student{
         string name;
         int roll;
     public:
-       void getData()
+       void getSid()
         {
-            cout << "Enter Data : \n" ;
             cout << "Enter Name : ";
             cin >> name;
             cout << "Enter Roll : ";
             cin >> roll;
             cout << endl;
         }
-};
-
-class Academic : public Student{
-    protected:
-        int m1, m2;
-    public:
-        void getData()
+        void DisplaySid()
         {
-            cout << "Enter Data : \n" ;
-            cout << "Enter 1st marks : ";
-            cin >> m1;
-            cout << "Enter 2nd marks : ";
-            cin >> m2;
+            cout << "\nName is :" << name;
+            cout << "\nRoll is :" << roll;
             cout << endl;
         }
 };
 
-class ExtraC : public Student{
+class Academic : virtual public Student{
     protected:
-        int e1, e2;
+        int m1, m2;
     public:
-        void getData()
+        void getAcd()
         {
-            cout << "Enter Data : \n" ;
-            cout << "Enter 1st marks(Extra) : ";
+            cout << "Enter  marks 1 : ";
+            cin >> m1;
+            cout << "Enter  marks 2: ";
+            cin >> m2;
+            cout << endl;
+        }
+        void DisplayAcd()
+        {
+            cout << "\n Marks 1 is :" << m1;
+            cout << "\n Marks 2 is :" << m2;
+            cout << endl;
+        }
+};
+
+class ExtraC : virtual public Student{
+    protected:
+        int e1;
+    public:
+        void getEx()
+        {
+            cout << "Enter Extra Marks: ";
             cin >> e1;
-            cout << "Enter 2nd marks(Extra) : ";
-            cin >> e2;
+            cout << endl;
+        }
+        void DisplayEx()
+        {
+            cout << "\n Extra Marks is :" << e1;
             cout << endl;
         }
 };
 
 class Result : public Academic, public ExtraC{
-
+    int Total;
     public:
         void Display()
         {
-           
+           Total = m1 + m2 + e1;
+           DisplaySid();
+           DisplayAcd();
+           DisplayEx();
+           cout << "\nTotal Marks is : " << Total;
+           cout << endl;
         }
 };
 
 int main()
 {
-    
-    cout << "enter name";
-    cout << "rr";
-    return 0;
+    int n,i;
+    cout << "\n Enter No. of students:";
+    cin >> n ;
+    Result s1[n];
+    cout <<"\nSTUDENT INPUTS\n";
+    for(i = 0 ; i < n ; i++)
+    {
+        cout << "\n Enter detail of Student No." << i+1 << ":\n";
+        s1[i].getSid(); 
+        s1[i].getAcd();
+        s1[i].getEx();
+    }
+    for ( i = 0; i < n; i++)
+    {
+        cout << "\n\n\t______XXXXXXXX_______\t" ;
+        s1[i].Display();
+    }
 }
--- C++ PROGRAMS/MultipleinH.cpp
+++ C++ PROGRAMS/MultipleinH.cpp
@@ -5,13 +5,19 @@ class Rectangle{
     protected:
         int a, b;
     public:
-        void getData()
+        void getDataR()
         {
-            cout << "Enter Data Rectangle : \n";
+            cout << "Enter Data of Rectangle : \n";
             cout << "Enter Width : ";
             cin >> a;
             cout << "Enter Length : ";
             cin >> b;
+            cout << endl;
+        }
+        void DisplayR()
+        {
+            cout << "\nWidth of the Rectangle : " << a;
+            cout << "\nHeight of the Rectangle : " << b;
             cout << endl;
         }
 };
@@ -20,7 +26,7 @@ class Triangle{
     protected:
         int c, d;
     public:
-        void getData()
+        void getDataT()
         {
             cout << "Enter Data of Triangle : \n";
             cout << "Enter Base : ";
@@ -29,14 +35,35 @@ class Triangle{
             cin >> d;
             cout << endl;
         }
+        void DisplayR()
+        {
+            cout << "\nBase of the Triangle : " << c;
+            cout << "\nHeight of the Triangle : " << d;
+            cout << endl;
+        }
 };
 
 class Area : public Rectangle, public Triangle{
+    int R, T;
     public:
         void getArea()
         {
+            getDataR();
+            getDataT();
+            R = a * b;
+            T = ((c * d) / 2);
             cout << "Area is : ";
-            cout << 
+            cout << "\nArea of Rectangle : " << R;
+            cout << "\nArea of Triangle : " << T;
+            cout << endl;
         }
+};
 
+int main ()
+{
+    Area A;
+    
+    A.getArea();
+
+    return 0;
 }
