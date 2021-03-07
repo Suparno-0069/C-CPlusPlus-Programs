2c6bd42dc8d3a28206e0b8ff215b59bb64df77da
Suparno-0069 <suparnochakraborty69@gmail.com>
2021-03-07 21:21:28 +0530

    07 MAR 2021

---
+++ C++ PROGRAMS/MultiLVLinH.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+class A{
+    protected:
+        int a, b;
+    public:
+        void getDataA()
+        {
+            cout << "Input one : ";
+            cin >> a >> b;
+            cout << endl;
+        }
+};
+
+class B : public A{
+    protected:
+        int g;
+    public:
+        void getData()
+        {
+            cout << "Input two : ";
+            cin >> g;
+            cout << endl;
+        }
+};
+
+class C : public B{
+    int f;
+    public:
+        void Display()
+        {
+            f = a + b + g;
+            cout << "Output one" << f;
+            cout << endl;
+        }
+};
+
+int main()
+{
+    C c1;
+    c1.getDataA();
+    c1.getData();
+    c1.Display();
+    return 0;
+}
